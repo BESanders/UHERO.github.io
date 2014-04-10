bf425be76c0c1e88f8c1f3b500cfb0efe2c98e43
DWang5090 <davidcxwang@hotmail.com>
2014-04-09 15:39:50 -1000

    Added slider

--- airfare/airfare.js
+++ airfare/airfare.js
@@ -90,14 +90,10 @@ function draw_map(column_name,data){
 var dates = [];
 d3.csv("Airfares_by_State.csv", function(data){
 	draw_map("1993Q1", data)
-	var keys = d3.keys(data[0]);
-	console.log(keys)
-	console.log(keys.length)
-	console.log(d3.keys(data[0]).length)
 	for(var i = 1; i < d3.keys(data[0]).length; i++){
-	// 	dates.push({i:d3.keys(data[0])[i]});
+		dates.push({i:d3.keys(data[0])[i]});
 	}
-	// console.log(dates)
+	console.log(dates)
 	$(function(){
 		$("#slider").slider({
 
